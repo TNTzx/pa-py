2cc784a09eb72461401fed959669afec14a1b56c
TNTzx <azelynnrituapersonality@gmail.com>
2023-08-17 13:30:23 +0800

    a

--- src/l_version/m_version.py
+++ src/l_version/m_version.py
@@ -1,18 +1,37 @@
 """Contains the version base class."""
 
+from __future__ import annotations
+
 import typing as typ
+import abc
 
 
 
 class LevelData():
     """Represents level data."""
 
+class CombineSettings():
+    """Represents combining settings."""
 
-T = typ.TypeVar("T")
-class Version(typ.Generic[T]):
+CombineSettingsT = typ.TypeVar("CombineSettingsT", CombineSettings)
+class Level(typ.Generic[CombineSettingsT], abc.ABC):
+    """Represents a PA level."""
+
+    @classmethod
+    @abc.abstractmethod
+    def combine_levels(
+        cls,
+        levels: list[Level],
+        primary_level: Level | None = None,
+        combine_settings: CombineSettingsT = CombineSettings()
+    ):
+        """
+        Combines levels into one level.
+        If provided, the primary level will be combined to the other levels and will keep all properties regardless of the combine settings.
+        """
+
+
+class Version():
     """Represents a PA version."""
     version_str: str
-    level_data_cls: type[T]
-
-    def __init__(self, level_data: T):
-        self.level_data = level_data
+    level_cls: type[Level]
